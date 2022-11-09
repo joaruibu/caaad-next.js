0d7f03b01d598d2d6a717b17a5fc8cddf5c8d609
Joaquín Ruiz Bueno <65560673+joaruibu@users.noreply.github.com>
2022-11-09 19:26:00 +0100

    Added vercel  analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/normalize.css'
 import '../styles/globals.css'
 
 import { ContextProvider } from '../context'
+import { Analytics } from '@vercel/analytics/react';
 
 
 function MyApp({ Component, pageProps }) {
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <ContextProvider >
       < Component {...pageProps} />
+      <Analytics />
     </ContextProvider>
   )
 }
