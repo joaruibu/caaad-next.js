c3e988bec1e7c1146aff77fe62b80f21ff3c3ab7
Joaquín Ruiz Bueno <65560673+joaruibu@users.noreply.github.com>
2023-06-20 18:01:56 +0200

    feature:premium-blocks

---
+++ components/SliderImg.js
@@ -0,0 +1,74 @@
+import Image from 'next/image';
+import React, { useState } from 'react';
+
+
+
+
+function SliderImg({ imgs }) {
+    const slides = [
+        {
+            url: 'https://res.cloudinary.com/dwsa2s0pn/video/upload/v1682344842/premium-autocad-block-mp4/Prueba_dybbmd.mp4'
+        },
+        {
+            url: 'https://res.cloudinary.com/dwsa2s0pn/video/upload/v1682344842/premium-autocad-block-mp4/Prueba_dybbmd.mp4'
+        }
+        // {
+        //     url: 'https://res.cloudinary.com/dwsa2s0pn/image/upload/v1671648010/free-autocad-block-image/Verner_Panton_-_Amoebe_seat_-_signature_furniture_-_perspective_-_autocad_block_-_bloque_de_autocad_-_be_interior_designer_ky5wcr.png',
+        // },
+        // {
+        //     url: 'https://res.cloudinary.com/dwsa2s0pn/image/upload/v1671696974/free-autocad-block-image/Verner_Panton_-_Cone_Chair_-_signature_furniture_-_elevation_-_autocad_block_-_bloque_de_autocad_-_be_interior_designer_zevewf.png',
+        // },
+        // {
+        //     url: 'https://res.cloudinary.com/dwsa2s0pn/image/upload/v1671735554/free-autocad-block-image/Verner_Panton_-_Cone_Chair_-_signature_furniture_-_side_view_-_autocad_block_-_bloque_de_autocad_-_be_interior_designer_ncop4r.png',
+        // },
+
+        // {
+        //     url: 'https://res.cloudinary.com/dwsa2s0pn/image/upload/v1671748171/free-autocad-block-image/Verner_Panton_-_Heart_Cone_Chair_-_signature_furniture_-_side_view_-_autocad_block_-_bloque_de_autocad_-_be_interior_designer_mvc0lo.png',
+        // },
+
+    ];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const prevSlide = () => {
+        const isFirstSlide = currentIndex === 0;
+        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+        setCurrentIndex(newIndex);
+    };
+
+    const nextSlide = () => {
+        const isLastSlide = currentIndex === slides.length - 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex);
+    };
+
+    return (
+
+        <div className='max-w-[1400px] h-[780px] w-full m-auto grid content-center relative group'>
+            {/* <div
+                style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+                className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+            ></div> */}
+            {/* <Image layout="responsive" width={250} height={165} alt={`Descargar bloque Autocad`} src={slides[currentIndex].url} priority className='duration-200' /> */}
+
+            <video autoPlay loop >
+                <source className='duration-200' src={slides[currentIndex].url} />
+            </video>
+            {/* <video src={slides[currentIndex].url}></video> */}
+            {/* Left Arrow */}
+            <div onClick={prevSlide} className='-rotate-180 hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+            </div>
+            {/* Right Arrow */}
+            <div onClick={nextSlide} className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+            </div>
+        </div >
+    );
+}
+
+export default SliderImg;
---
+++ pages/premium-autocad-block/[id].js
@@ -0,0 +1,272 @@
+
+import React from "react";
+import dbConnect from "../../lib/dbConnect";
+import Block from "../../models/Block";
+import Layout from "../../components/Layout";
+import AddSidebar from "../../components/AddSidebar";
+import Image from "next/image";
+
+import Link from "next/link";
+import BadgeTag from "../../components/BadgeTag";
+import FilterTag from "../../components/BadgeFilter"
+import UrlNotFound from "../404";
+import SimilarBlock from "../../components/SimilarBlock";
+import { useRouter } from "next/router";
+import BadgeCategory from "../../components/BadgeCategorie";
+import SliderImg from "../../components/SliderImg";
+import { urlTitle } from "../../helpers";
+
+
+const BlockPage = ({ success, error, block, similarBlocks }) => {
+  console.log(block)
+
+  const { _id, dwg, description, description_ES, categories, filters, img, tags, title, title_ES } = block
+  const { locale } = useRouter()
+  console.log(_id)
+
+  // Función de prueba para simular la pagina despues del pago de Stripe
+  const getUrlAfterPayment = () => {
+
+    return `/download-premium-autocad-block/${urlTitle(title)}-${_id.split('').reverse().join('')}`
+  }
+
+
+
+  useRouter()
+  if (!success) {
+    return <UrlNotFound error={error} />
+  }
+
+  return (
+
+    <Layout
+      pagina={locale === 'es' ? `Descargar bloque de autocad ${title_ES}` : `Download ${title} cad block`}>
+
+      <main className='grid grid-cols-1 gap-9 md:grid-cols-[160px_1fr_160px] content-start md:gap-9 min-h-screen'>
+        <div className='hidden md:block md:col-start-1'>
+          {/* <AddSidebar /> */}
+        </div>
+        <div className='md:col-start-2 md:col-end-3 grid grid-rows-none grid-cols-1 xl:grid-cols-2 gap-12'>
+          <div className="relative border border-orange-600 rounded-3xl bg-white overflow-hidden grid content-center">
+            {/* <Image layout="responsive" width={250} height={165} alt={locale === 'es' ? `Descargar bloque autoCAD ${title_ES}` : `Download ${title}  cad block`} src={img} priority />
+             */}
+            <SliderImg></SliderImg>
+          </div>
+          <div>
+            <h1 className="text-5xl">Bloques de pagoooooooooo</h1>
+            <div className="mb-4">
+              <p className="font-bold block w-full">{locale === 'es' ? `Título:` : `Title:`} </p>
+              <h1 className=" font-bold text-xl text-orange-500">{locale === 'es' ? `Descargar bloque autoCAD ${title_ES}` : `Download cad block ${title}`}.</h1>
+            </div>
+            <div className="mb-4">
+              <p className="font-bold block w-full">{locale === 'es' ? `Descripción:` : `Description:`} </p>
+              <p className=" font-normal text-gray-500">{locale === 'es' ? `${description_ES}` : `${description}`}</p>
+            </div>
+            <div className="mb-4">
+              <p className="mb-3 font-bold block w-full">{locale === 'es' ? `Categoría:` : `Category:`} </p>
+              <div className="flex flex-wrap gap-2">
+
+                {categories.map((category, index) => {
+                  return <BadgeCategory
+                    key={index}
+                    category={category} />
+                })}
+
+              </div>
+            </div>
+
+            <div className="mb-8">
+              <p className="mb-3 font-bold block w-full">{locale === 'es' ? `Filtros:` : `Filters:`}  </p>
+              <div className="flex flex-wrap gap-2">
+                {tags.map((tag, index) => {
+                  return <BadgeTag
+                    key={index}
+                    tag={tag}
+                    icon={false}
+                  />
+                })
+                }
+                {filters.map((filter, index) => {
+                  return <FilterTag
+                    key={index}
+                    filter={filter}
+                    icon={false}
+                  />
+                })
+                }
+              </div>
+            </div>
+            <Link href={getUrlAfterPayment()} passHref={true} >
+              <a className="cursorHover w-full block rounded-full text-lg uppercase text-center p-3 bg-orange-600 hover:bg-orange-700 transition-all text-white">{locale === 'es' ? `Descargar bloque de autoCAD gratis` : `Download free CAD block`} </a>
+            </Link>
+          </div>
+        </div>
+
+        <div className='hidden md:block  md:col-start-3'>
+          {/* <AddSidebar /> */}
+        </div>
+        <div className='block  md:col-start-2 w-full mt-12'>
+          <SimilarBlock similarBlocks={similarBlocks} />
+        </div>
+
+
+      </main>
+    </Layout>
+  )
+};
+
+
+
+export default BlockPage;
+
+
+export async function getStaticPaths() {
+
+  try {
+    await dbConnect()
+    const result = await Block.find({ 'free': false })
+
+    // Mapeo todos los objetos del array para 'limpiar' la propiedad _id
+    const blocks = result.map((doc) => {
+      const block = doc.toObject()
+      block._id = block._id.toString()
+      return block
+    })
+
+    const paths = blocks.slice(0, 20).map(({ _id, title }) => {
+      const splitTitle = title.replaceAll(' ', '-');
+      return {
+        params: { id: `${splitTitle.toLowerCase()}-${_id}` },
+        locale: 'en'
+      }
+    });
+
+    const paths_ES = blocks.slice(0, 20).map(({ _id, title_ES }) => {
+      const splitTitle = title_ES.replaceAll(' ', '-');
+      return {
+        params: { id: `${splitTitle.toLowerCase()}-${_id}` },
+        locale: 'es'
+      }
+    });
+
+    return {
+      paths: [...paths, ...paths_ES],
+      fallback: 'blocking'
+    }
+
+  } catch (error) {
+
+  }
+}
+
+export async function getStaticProps({ params: { id } }) {
+  //la Url está compuesta por título - id
+  const newId = id.split('-').pop();
+
+  try {
+    await dbConnect()
+    const block = await Block.findById(newId).lean()
+    block._id = block._id.toString()
+
+    // Buscar como hacer la busqueda del titulo similar al actual
+    const similarResults = (await Block.find({ tags: block.tags }).limit(10)).reverse();
+
+    const similarBlocks = similarResults
+      .map((ele) => {
+        const block = ele.toObject()
+        block._id = block._id.toString()
+        return block
+      })
+      // Eliminamos el bloque en el que estamos
+      .filter(ele => ele._id !== newId)
+
+    if (!block)
+      return {
+        props: {
+          success: false,
+          error: 'Bloque no encontrado!'
+        }
+      }
+
+    return {
+      props: {
+        success: true,
+        block,
+        similarBlocks
+      }
+    }
+
+  } catch (error) {
+    console.log(error)
+    if (error.kind === 'ObjectId') {
+      return { props: { success: false, error: 'Te has invetando la dirección' } }
+    }
+    return { props: { success: false, error: 'Error de servidor' } }
+  }
+}
+
+
+// Codigo previo con GSSP que funciona 
+// export async function getServerSideProps({ params }) {
+//   console.log(params)
+//   //la Url está compuesta por título - id
+//   const splitUrl = params.id.split("-")
+//   const id = splitUrl[splitUrl.length - 1]
+
+//   try {
+//     await dbConnect()
+
+//     const block = await Block.findById(id).lean()
+//     block._id = block._id.toString()
+
+
+//     if (!block)
+//       return {
+//         props: {
+//           success: false,
+//           error: 'Bloque no encontrada!'
+//         }
+//       }
+
+//     return {
+//       props: {
+//         success: true,
+//         block
+//       }
+//     }
+
+//   } catch (error) {
+//     console.log(error)
+//     if (error.kind === 'ObjectId') {
+//       return { props: { success: false, error: 'Te has invetando la dirección' } }
+//     }
+//     return { props: { success: false, error: 'Error de servidor' } }
+//   }
+// }
+
+
+// Esto funciona
+// export async function getServerSideProps(context) {
+//   console.log(2222, context.query)
+//   try {
+//     await dbConnect()
+//     // Busco por router.query.title para evitar problemas en el caso de reload y acceder directamente a una URL
+//     const block = await Block.findById(context.query.id).lean()
+
+//     block._id = ` ${block._id}`
+//     if (!block)
+//       return { props: { success: false, error: 'Bloque no encontrada!' } }
+
+//     return {
+//       props: { success: true, block }
+//     }
+
+//   } catch (error) {
+//     console.log(error)
+//     if (error.kind === 'ObjectId') {
+//       return { props: { success: false, error: 'Te has invetando la dirección' } }
+//     }
+//     return { props: { success: false, error: 'Error de servidor' } }
+//   }
+// }
+
