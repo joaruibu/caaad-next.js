9ddccfec9feee804fe5b0d4e9c30bb35503abc11
Joaquín Ruiz <joaquinruizbueno@gmail.com>
2024-10-05 22:02:02 +0100

    chore: added google analitycs

--- components/Layout.js
+++ components/Layout.js
@@ -1,56 +1,51 @@
-import Script from 'next/script'
-import Head from 'next/head'
-import React from 'react'
-import Footer from './Footer'
-import Header from './Header'
-import CookiesBanner from '../components/CookiesBanner'
+import Script from "next/script";
+import Head from "next/head";
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import CookiesBanner from "../components/CookiesBanner";
 
-import { useApp } from '../context'
+import { useApp } from "../context";
+import GoogleAnalytics from "./Analitycs";
 // import SuscriptionBanner from './SuscriptionBanner'
 
-
 const Layout = ({ children, pagina }) => {
-    const { cookiesAccepted, locale } = useApp()
-    const title = `${pagina}`
-    return (
-        <>
-            <Head>
-                <title>{title}</title>
-                <meta name="description" content={locale === 'es' ? 'Descarga bloques de autocad gratis, descarga los mejores bloques de autocad.' : 'Dowload free autocad blocks, download the best autocad block'} />
-                <meta httpEquiv="content-language" content={locale}></meta>
-                <meta name='robots' content='index, follow'></meta>
-                <link rel="icon" href="/favicon.png" />
-            </Head>
-            <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-                strategy='afterInteractive' />
+  const { cookiesAccepted, locale } = useApp();
+  const title = `${pagina}`;
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta
+          name="description"
+          content={
+            locale === "es"
+              ? "Descarga bloques de autocad gratis, descarga los mejores bloques de autocad."
+              : "Dowload free autocad blocks, download the best autocad block"
+          }
+        />
+        <meta httpEquiv="content-language" content={locale}></meta>
+        <meta name="robots" content="index, follow"></meta>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <GoogleAnalytics />
 
-            <Script
-                id='GA'
-                strategy='afterInteractive'
-                dangerouslySetInnerHTML={{
-                    __html: `window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      
-      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`
-                }} />
+      <div className="container mx-auto px-3">
+        <Header />
+        {children}
+        {cookiesAccepted === false ? <CookiesBanner /> : ""}
+        {/* <SuscriptionBanner /> */}
 
-            <div className='container mx-auto px-3'>
+        <Footer />
+        <Script
+          src="https://kit.fontawesome.com/52fed5dcd0.js"
+          crossOrigin="anonymous"
+          async
+          strategy="lazyOnload"
+        />
+      </div>
+    </>
+  );
+};
 
-
-                <Header />
-                {children}
-                {cookiesAccepted === false ? <CookiesBanner /> : ''
-
-                }
-                {/* <SuscriptionBanner /> */}
-
-                <Footer />
-                <Script src="https://kit.fontawesome.com/52fed5dcd0.js" crossOrigin="anonymous" async strategy="lazyOnload" />
-            </div>
-        </>
-    )
-}
-
-export default Layout
+export default Layout;
