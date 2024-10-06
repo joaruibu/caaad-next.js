76481da26dfc62951359b802ddf3af24be5ee77a
Joaquín Ruiz <joaquinruizbueno@gmail.com>
2024-10-06 08:25:03 +0100

    chore: fix env name analytics

--- components/Analitycs.js
+++ components/Analitycs.js
@@ -4,11 +4,12 @@ import React from "react";
 import Script from "next/script";
 
 const GoogleAnalytics = () => {
+  console.log(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS);
   return (
     <>
       <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
       <Script id="" strategy="lazyOnload">
@@ -16,7 +17,7 @@ const GoogleAnalytics = () => {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
               page_path: window.location.pathname,
               });
           `}
